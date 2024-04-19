bd49f83711e86ca4aa6e1a41c8328f8bde5f1a22
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-04-19 23:09:23 +0900

    prisma利用してDBにデータが保存できるか確認した

---
+++ src/script.js
@@ -0,0 +1,24 @@
+// DBにアクセスするためのクライアントライブラリ
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient()
+
+async function main() {
+    const newLink = await prisma.link.create(
+        {
+            data: {
+                description: "GraphQLを使用したNewsAppのためのWebAPI作成要領",
+                url: "https://news.ycombinator.com/"
+            }
+        }
+    );
+
+    const allLinks = await prisma.link.findMany();
+    console.log(allLinks);
+}
+
+main().catch((e) => {
+    throw e;
+}).finally(async () => {
+    prisma.$disconnect;
+})
