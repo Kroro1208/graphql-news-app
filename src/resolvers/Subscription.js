a88d46f57f6e04da17d1aaf7ffb577e7fc1465fb
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-19 21:28:08 +0900

    newLinkSubscribe実装した

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,14 @@
+function newLinkSubscribe(parent, args, context) {
+    return context.pubsub.asyncIterator("NEW_LINK");
+}
+
+const newLink = {
+    sebscribe: newLinkSubscribe,
+    resolve: (payload) => {
+        return payload;
+    }
+};
+
+module.exports = {
+    newLink
+};
